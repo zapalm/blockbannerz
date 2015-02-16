081d9db1f7eef502f95c15a3382877870fe6a97a
zapalm <zapalm@ya.ru>
2015-02-16 13:21:16 +1000

    [*] TR : default theme

--- translations/ru.php
+++ translations/ru.php
@@ -2,5 +2,5 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{blockbannerz}default-bootstrap>blockbannerz_8214418c85d872d6ce91f35a136945e5'] = 'Блок баннеров';
-$_MODULE['<{blockbannerz}default-bootstrap>blockbannerz_91cd1ee56ea5324ff51578684a393a81'] = 'Добавляет блоки с рекламными баннерами в указанные места сайта';
+$_MODULE['<{blockbannerz}prestashop>blockbannerz_8214418c85d872d6ce91f35a136945e5'] = 'Блок баннеров';
+$_MODULE['<{blockbannerz}prestashop>blockbannerz_91cd1ee56ea5324ff51578684a393a81'] = 'Добавляет блоки с рекламными баннерами в указанные места сайта';
