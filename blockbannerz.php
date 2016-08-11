0c4cd97602839ad48d95ebdcba74123b430b5e4c
zapalm <zapalm@ya.ru>
2016-08-11 21:15:27 +1000

    // license header format

--- blockbannerz.php
+++ blockbannerz.php
@@ -2,10 +2,10 @@
 /**
  * Block Banners: module for PrestaShop 1.5-1.6
  *
- * @author zapalm <zapalm@ya.ru>
+ * @author    zapalm <zapalm@ya.ru>
  * @copyright (c) 2013-2015, zapalm
- * @link http://prestashop.modulez.ru/en/ Homepage
- * @license http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
+ * @link      http://prestashop.modulez.ru/en/ Homepage
+ * @license   http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
  */
 
 if (!defined('_PS_VERSION_'))
